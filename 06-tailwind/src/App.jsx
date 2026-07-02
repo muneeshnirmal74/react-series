eed757e7de4fd869474c32f68def3f6ee22a64cc
Muneesh Nirmal <muneeshnirmal74@gmail.com>
2026-07-02 23:53:48 +0530

    Added 06 Tailwind Project

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-center text-blue-500">
+        Hello, Tailwind CSS!
+      </h1>
+    </div>
+  )
+}
+
+export default App
