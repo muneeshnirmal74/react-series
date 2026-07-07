9e9a5c1d34600d9fb883c7c692fed97862d77b19
Muneesh <muneeshnirmal@everymedia.world>
2026-07-07 13:57:01 +0530

    Local Storage Don

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function App() {
+  // localStorage.clear();
+  // localStorage.setItem("Mukesh", "kam nhi kr raha");
+
+  // const userName = localStorage.getItem("Mukesh");
+  // console.log(userName);
+
+  // localStorage.removeItem("Mukesh");
+
+  // obj ke bhejne ke liye json.s
+
+  const user = {
+    name: "Muneesh",
+    age: 29,
+    location: "Vile parle",
+  };
+
+  // localStorage.setItem("user", JSON.stringify(user));
+
+  // const data = localStorage.getItem(user);
+  // const updatedData = JSON.stringify(user);
+  // console.log(updatedData);
+
+  const usera = JSON.parse(localStorage.getItem("user"));
+  console.log(usera);
+
+  return <div>Hello</div>;
+}
+
+export default App;
