5ee448b65920a256d5f4470bdf6216a2cbe40394
Muneesh <muneeshnirmal@everymedia.world>
2026-07-07 16:41:43 +0530

    15 API Calling

--- 15-api-calling/src/App.jsx
+++ 15-api-calling/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
   // async function btnClick() {
@@ -8,14 +9,38 @@ function App() {
   //   console.log(data);
   // }
 
+  const [data, setData] = useState([]);
+
   const getData = async () => {
     const responce = await axios.get("https://picsum.photos/v2/list");
-    console.log(responce.data);
+
+    setData(responce.data);
+    console.log(data);
   };
 
   return (
     <div>
       <button onClick={getData}>Click</button>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: "30px",
+        }}
+      >
+        {data.map((val, key) => {
+          return (
+            <div key={key}>
+              <h2>{val.author}</h2>
+
+              <img src={val.download_url} style={{ width: "100%" }} />
+              <p>Id :{val.id}</p>
+              <a href={val.url}>Url</a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
